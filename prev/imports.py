86071d9d5f19924e52ed6441abd7a571e42aa414
Eric Riddoch <eric.russia97@gmail.com>
2023-04-07 17:17:09 -0600

    committing imports.py

--- prev/imports.py
+++ prev/imports.py
@@ -1,9 +1,4 @@
-from __future__ import absolute_import, annotations
-
-from typing import Dict, NamedTuple, Optional, Set
-
-import numpy as np
-from black import Cache
-from pylint import lint
-
-from .linting import Book
+from __future__ import (
+    absolute_import,
+    annotations,
+)
