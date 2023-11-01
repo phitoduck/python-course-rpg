8138ab34f248954f43b2b894f082c759517d72f6
Sagar <sagardesaee@gmail.com>
2023-11-01 16:19:46 +0530

    another function added

--- example.py
+++ example.py
@@ -5,3 +5,9 @@
 def add_numbers(a: int, b: int) -> int:
     """Return the sum of two numbers."""
     return sum([a, b])
+
+
+# pylint: disable=invalid-name
+def add_numbers2(a: int, b: int) -> int:
+    """Return the sum of two numbers."""
+    return sum([a, b])
